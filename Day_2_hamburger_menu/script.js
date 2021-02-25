3de606e747167a67e02a12b8df726467dc9fb30c
Miki <paszkowski.mikolaj98@gmail.com>
2021-02-25 10:29:06 +0100

    day-3-circles

--- Day_2_hamburger_menu/script.js
+++ Day_2_hamburger_menu/script.js
@@ -1,14 +1,12 @@
 
 
-let upperLine = document.getElementById('line-1');
-let lowerLine = document.getElementById('line-3');
-let middleLine = document.getElementById('line-2');
-let centerContainer = document.querySelector('.center');
-//let centerContainer = document.querySelector('.line-1-rotate');
-
-
 
 centerContainer.addEventListener('click', (e) => {
+
+    let upperLine = document.getElementById('line-1');
+    let lowerLine = document.getElementById('line-3');
+    let middleLine = document.getElementById('line-2');
+    let centerContainer = document.querySelector('.center');
 
     upperLine.classList.toggle('line-1-rotate');
     lowerLine.classList.toggle('line-3-rotate');
